797e65971f4f4e29b9582c8a7fff269fe5da05de
Tyler.Hoffmann <tyler.hoffmann5654@gmail.com>
2025-03-25 11:47:03 -0400

    project update

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function setButtonText(
+  btn,
+  isLoading,
+  defaultText = "Save",
+  loadingText = "Saving..."
+) {
+  if (isLoading) {
+    btn.textContent = loadingText;
+  } else {
+    // set not loading text
+    btn.textContent = defaultText;
+  }
+}
